16ef3a71e6a1a5746d20256a8b55486fb547e7ac
Hyunsoo_kim <red_panda119@naver.com>
2026-05-23 01:28:59 +0900

    feat : 스킬 아이콘 이미지 맵 추가

---
+++ src/consts/skillIcons.ts
@@ -0,0 +1,23 @@
+export const FE_ICONS = [
+  { src: "/icons/html.svg", alt: "html"},
+  { src: "/icons/css.svg", alt: "css"},
+  { src: "/icons/sass.svg", alt: "scss" },
+  { src: "/icons/javascript.svg", alt: "javascript"},
+  { src: "/icons/typescript.svg", alt: "typescript" },
+  { src: "/icons/tailwindcss.svg", alt: "tailwindcss" },
+  { src: "/icons/react.svg", alt: "react" },
+  { src: "/icons/nextjs.svg", alt: "nextjs" },
+  { src: "/icons/tanstackquery.svg", alt: "tanstackquery" },
+];
+
+export const BE_ICONS = [
+  { src: "/icons/c.svg", alt: "c" },
+  { src: "/icons/java.svg", alt: "java" },
+  { src: "/icons/nodejs.svg", alt: "nodejs" },
+  { src: "/icons/expressjs.svg", alt: "expressjs" },
+  { src: "/icons/fastify.svg", alt: "fastify" },
+  { src: "/icons/nestjs.svg", alt: "nestjs" },
+  { src: "/icons/mongodb.svg", alt: "mongodb" },
+  { src: "/icons/postgresql.svg", alt: "postgresql" },
+  { src: "/icons/git.svg", alt: "git" },
+];
