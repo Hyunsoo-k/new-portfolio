196daf235001dd3d4f3c0a78fdf23285c85f6fb9
Hyunsoo_kim <red_panda119@naver.com>
2026-05-23 02:48:16 +0900

    chore : 정적 이미지 파일명, 경로 수정

--- src/consts/skillIcons.ts
+++ src/consts/skillIcons.ts
@@ -1,23 +1,23 @@
 export const FE_ICONS = [
-  { src: "/icons/html.svg", alt: "html"},
-  { src: "/icons/css.svg", alt: "css"},
+  { src: "/icons/html5.svg", alt: "html5"},
+  { src: "/icons/css3.svg", alt: "css3"},
   { src: "/icons/sass.svg", alt: "scss" },
   { src: "/icons/javascript.svg", alt: "javascript"},
   { src: "/icons/typescript.svg", alt: "typescript" },
-  { src: "/icons/tailwindcss.svg", alt: "tailwindcss" },
+  { src: "/icons/tailwindCss.svg", alt: "tailwindCss" },
   { src: "/icons/react.svg", alt: "react" },
-  { src: "/icons/nextjs.svg", alt: "nextjs" },
-  { src: "/icons/tanstackquery.svg", alt: "tanstackquery" },
+  { src: "/icons/nextJs.svg", alt: "nextJs" },
+  { src: "/icons/tanstackQuery.svg", alt: "tanstackQuery" },
 ];
 
 export const BE_ICONS = [
   { src: "/icons/c.svg", alt: "c" },
   { src: "/icons/java.svg", alt: "java" },
-  { src: "/icons/nodejs.svg", alt: "nodejs" },
-  { src: "/icons/expressjs.svg", alt: "expressjs" },
+  { src: "/icons/nodeJs.svg", alt: "nodejs" },
+  { src: "/icons/expressJs.svg", alt: "expressJs" },
   { src: "/icons/fastify.svg", alt: "fastify" },
-  { src: "/icons/nestjs.svg", alt: "nestjs" },
-  { src: "/icons/mongodb.svg", alt: "mongodb" },
+  { src: "/icons/nestJs.svg", alt: "nestJs" },
+  { src: "/icons/mongoDb.svg", alt: "mongoDb" },
   { src: "/icons/postgresql.svg", alt: "postgresql" },
   { src: "/icons/git.svg", alt: "git" },
 ];
