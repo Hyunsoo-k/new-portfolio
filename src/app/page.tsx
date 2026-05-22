c1c6ab8daba7f5c01008b30cd76c35a40917ee0d
Hyunsoo_kim <red_panda119@naver.com>
2026-05-23 01:31:58 +0900

    feat : HeroSection 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { HeroSection } from "@/components/HeroSection/HeroSeciotn";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      
+      <HeroSection />
     </div>
   );
 }
