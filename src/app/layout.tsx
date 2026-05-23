f42af675b05762d17a39e356a3f522cc6559a18a
Hyunsoo_kim <red_panda119@naver.com>
2026-05-24 05:29:26 +0900

    feat : meta 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ import "./globals.scss";
 export const metadata: Metadata = {
   title: "HYUNSOO KIM",
   description: "Fullstack developer portfolio.",
+  keywords: ["풀스택 개발자", "개발자 포트폴리오", "김현수", "Next.js 포트폴리오", "React", "Fullstack Developer"],
+  robots: { index: true, follow: true },
 };
 
 const pretendard = localFont({
