8f871f40417539f7e6504986668d3044c05196e3
Hyunsoo_kim <red_panda119@naver.com>
2026-05-24 04:35:13 +0900

    feat : 각 컴포넌트에 div wrapper 생성

--- src/components/sections/ContactSection/ContactSection.tsx
+++ src/components/sections/ContactSection/ContactSection.tsx
@@ -5,12 +5,14 @@ import styles from "./ContactSection.module.scss";
 
 export const ContactSection = () => {
   return (
-    <SectionLayout
-      title="Contact"
-      description="새로운 프로젝트 제안이나 협업 문의는 언제든 환영합니다. 편하게 연락주세요."
-      isBgSubtle={false}
-    >
-      <Contact />
-    </SectionLayout>
+    <div className={styles.contactSection}>
+      <SectionLayout
+        title="Contact"
+        description="새로운 프로젝트 제안이나 협업 문의는 언제든 환영합니다. 편하게 연락주세요."
+        isBgSubtle={false}
+      >
+        <Contact />
+      </SectionLayout>
+    </div>
   );
 };
