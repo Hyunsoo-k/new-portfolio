f3f3ae007484bcf9effd5a1a45f840637efba9be
Hyunsoo_kim <red_panda119@naver.com>
2026-05-24 01:46:23 +0900

    feat : SkillSection 컴포넌트 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { HeroSection } from "@/components/sections/HeroSection/HeroSeciotn";
 import { ProjectsSection } from "@/components/sections/ProjectsSection/ProjectsSection";
+import { SkillsSection } from "@/components/sections/SkillSection/SkillsSection";
 
 import styles from "./page.module.scss";
 
@@ -8,6 +9,7 @@ export default function Home() {
     <div className={styles.page}>
       <HeroSection />
       <ProjectsSection />
+      <SkillsSection />
     </div>
   );
 }
