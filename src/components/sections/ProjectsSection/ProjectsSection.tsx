3cfc058489e4c546b262565df2010347460dcae0
Hyunsoo_kim <red_panda119@naver.com>
2026-05-24 01:45:18 +0900

    feat : SectionLayout Prop 값 추가

--- src/components/sections/ProjectsSection/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection/ProjectsSection.tsx
@@ -5,7 +5,11 @@ import styles from "./ProjectsSection.module.scss";
 
 export const ProjectsSection = () => {
   return (
-    <SectionLayout title="Projects" description="카드를 클릭하고 프로젝트 정보를 확인해 보세요.">
+    <SectionLayout
+      title="Projects"
+      description="카드를 클릭하고 프로젝트 정보를 확인해 보세요."
+      isBgSubtle={false}
+    >
       <ProjectCardList />
     </SectionLayout>
   );
